43376233f331b1b777ce63ac3f19f10108fb48d3
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2024-04-07 20:30:49 -0700

    Removed member variable and just parameterized loadModule()'s necessary directory

--- src/ModuleManager.h
+++ src/ModuleManager.h
@@ -2,18 +2,22 @@
 
 #include "Logger.h"
 #include "Module.h"
+#include "TextManipulations.h"
 
 #include <string>
 #include <memory>
 #include <unordered_map>
 #include <vector>
+#include <filesystem>
+
+#include <iostream>
 
 using StringVector = std::vector<std::string>;
 
 
 class ModuleManager final {
 public:
-    ModuleManager(std::string setModduleDirectory, std::shared_ptr<Logger> setLogger);
+    ModuleManager(std::shared_ptr<Logger> setLogger);
     ~ModuleManager();
 
     bool addModule(ModulePtr module);
@@ -22,13 +26,12 @@ public:
     ModulePtr   getModule(std::string title) const;
     bool        containsModule(std::string title) const;
 
-    std::pair<int, StringVector> loadModules();
+    std::pair<int, StringVector> loadModules(std::string directory);
 
 private:
 
     std::unordered_map<std::string, ModulePtr> modules{};
 
-    std::string moduleDirectory{};
     std::shared_ptr<Logger> logger;
 
 
