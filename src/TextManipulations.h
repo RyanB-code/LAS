703a7ceab52330fcdc07cfe82d04296f386624e0
Bradley Ryan <ryan.bradley.e@gmail.com>
2024-04-23 19:28:20 -0700

    Basic formatting for Source Locations and Timepoints added

--- src/TextManipulations.h
+++ src/TextManipulations.h
@@ -1,6 +1,13 @@
 #pragma once
 #include <string>
+#include <chrono>
+#include <source_location>
 
-namespace TextManipulations{
+namespace TextManipulations {
     std::string ensureSlash(std::string text);
+
+    namespace Logging{
+        std::string printTime(const std::chrono::system_clock::time_point& time)    noexcept;   // HH:MM:SS
+        std::string printLocation(const std::source_location& location)             noexcept;   // <File Name> | <Function Name> | <Line> 
+    }
 }
