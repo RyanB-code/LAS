ae798c24812d657e9a80b804f9af4be97701fab6
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2024-04-23 19:28:20 -0700

    Basic formatting for Source Locations and Timepoints added

--- src/TextManipulations.cpp
+++ src/TextManipulations.cpp
@@ -9,3 +9,17 @@ namespace TextManipulations{
         return text;
     }
 }
+
+namespace TextManipulations::Logging{
+
+    std::string printTime(const std::chrono::system_clock::time_point& time) noexcept {
+        return std::format("{:%OH:%OM:%OS}", time);
+    }
+    std::string printLocation(const std::source_location& location)	noexcept {
+        std::ostringstream os;
+
+        os << "[" << location.file_name() << " | " << location.function_name() << " | " << location.line() << "]";
+
+        return os.str();
+    }
+}
