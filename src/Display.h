dd9497a8af812f6dd8a001a9d410be05647a7529
Bradley Ryan <ryan.bradley.e@gmail.com>
2024-04-23 19:29:56 -0700

    Created basic Display type

---
+++ src/Display.h
@@ -0,0 +1,33 @@
+#include "Logging.h"
+
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
+#include <GLFW/glfw3.h>
+
+#include </usr/include/GL/gl.h>
+#include </usr/include/GL/glx.h>
+#include </usr/include/GL/glu.h>
+#include </usr/include/GL/glext.h>
+#include </usr/include/GL/glxext.h>
+
+class DisplayManager;
+
+using DisplayManagerPtr = std::shared_ptr<DisplayManager>;
+
+class DisplayManager{
+public:
+    DisplayManager(const Logger& setLogger);
+    ~DisplayManager();
+
+    bool init();
+    bool refresh();
+
+private:
+    bool initGLFW();
+    bool initImgui();
+    bool initOpenGL();
+
+    const Logger& logger;
+};
