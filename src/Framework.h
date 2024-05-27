3c427553207a83645bdcf72ef00bb2783a67e36a
Bradley Ryan <ryan.bradley.e@gmail.com>
2024-05-26 19:50:04 -0700

    WIP building framework
    Added correct sequence of setup for commands and the configuratio file, added helper functions, still flushing out basic Logging setup

--- src/Framework.h
+++ src/Framework.h
@@ -1,19 +1,53 @@
+#pragma once
+
 #include "ModuleManager.h"
 #include "Logging.h"
 #include "Display.h"
+#include "Commands.h"
 
-class Framework {
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <queue>
+
+class Framework final{
 public:
-    Framework (ModuleManagerPtr setModuleManager, LoggerPtr setLogger, DisplayManagerPtr setDisplayManager);
-    ~Framework ();
+    Framework(  LoggerPtr           setLogger=nullptr,
+                ModuleManagerPtr    setModuleManager=nullptr,
+                DisplayManagerPtr   setDisplayManager=nullptr
+            );
+    ~Framework  ();
 
     bool setup();
+    void run();
 
 private:
-    ModuleManagerPtr moduleManager;
     LoggerPtr logger;
+    ModuleManagerPtr moduleManager;
     DisplayManagerPtr displayManager;
 
+    std::unordered_map<std::string, CommandPtr> commands;
+    std::queue<std::string> commandQueue;  
 
-    bool setupDisplay();
+    void setupCommands();
+    bool addCommand(std::unique_ptr<Command> command);
+    bool readSetupFile(std::string path);
+    bool handleCommandQueue();
+
 };
+
+// For testing command functionality
+class TestCommand : public Command{
+public: 
+    TestCommand();
+    ~TestCommand();
+
+    bool execute() const override;
+};
+
+namespace LAS {
+    bool ensureDirectory    (std::string path);
+    bool ensureFile         (std::string path);
+
+    std::string getParentDir();
+}
