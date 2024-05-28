d52f306d677a63a0719e4370bd4c4eaf082459f2
Bradley Ryan <ryan.bradley.e@gmail.com>
2024-05-27 20:29:07 -0700

    Output uses zoned time and seconds only as 2 digits instead of a float

--- src/TextManipulations.cpp
+++ src/TextManipulations.cpp
@@ -13,7 +13,14 @@ namespace TextManipulations{
 namespace TextManipulations::Logging{
 
     std::string printTime(const std::chrono::system_clock::time_point& time) noexcept {
-        return std::format("{:%OH:%OM:%OS}", time);
+        std::chrono::zoned_time zonedTime { std::chrono::current_zone(), time }; // Makes zoned time
+
+        // Needed to output the seconds as 2 digits instead of a float
+        std::string seconds {std::format("{:%S}", zonedTime)};
+        std::ostringstream formattedTime;
+        formattedTime << std::format("{:%H:%M:}", zonedTime) << seconds.substr(0,2);
+
+        return formattedTime.str();
     }
     std::string printLocation(const std::source_location& location)	noexcept {
         std::ostringstream os;
