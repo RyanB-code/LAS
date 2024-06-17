51f15b2b8e760c9fef3d4a71ea8f9ce9e5ba040c
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2024-06-17 13:59:02 -0700

    Reinstated const for LogOutput's logging function

--- Environment/src/LoggingInternal.h
+++ Environment/src/LoggingInternal.h
@@ -9,7 +9,7 @@ public:
     std::string getPath() const;
     bool setPath(std::string setPath);      // Sets the path to the file, will not create file
 
-    bool log (const Log& log, const LogSettings& logSettings) override;
+    bool log (const Log& log, const LogSettings& logSettings) const override;
 private:
     std::string path;
 };
@@ -33,7 +33,7 @@ public:
     LogToWindow(std::shared_ptr<LogWindow>);
     ~LogToWindow();
 
-    bool log(const Log&, const LogSettings&) override;
+    bool log(const Log&, const LogSettings&) const override;
     std::shared_ptr<LogWindow> getWindow();
 
 private:
