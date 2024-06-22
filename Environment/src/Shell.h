20f42a92586698ca29f4a6fa63791381687f188b
Bradley Ryan <ryan.bradley.e@gmail.com>
2024-06-22 09:48:08 -0700

    Moved Console object into Shell.h, Implemented Shell class

---
+++ Environment/src/Shell.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "Display.h"
+#include "ModuleManager.h"
+
+#include <imgui/imgui.h>
+#include <LAS/Commands.h>
+#include <LAS/Helper.h>
+#include <LAS/Window.h>
+
+#include <string>
+#include <map>
+#include <queue>
+#include <memory>
+#include <vector>
+
+// FOR TESTING
+#include <iostream>
+
+// Forward Declarations
+namespace LAS{
+    class ShellOutput;
+    class Shell;
+}
+
+using ShellPtr = std::shared_ptr<LAS::Shell>;
+using ShellOutputPtr = std::unique_ptr<LAS::ShellOutput>;
+
+namespace LAS{
+
+    class ConsoleWindow : public LAS::Window{
+    public:
+        ConsoleWindow(std::queue<std::string>& setQueue);
+        ~ConsoleWindow();
+
+        void draw() override;
+
+    private:
+        std::queue<std::string>& queue;
+    };
+
+
+    class ShellOutput{
+    public:
+        ShellOutput();
+        virtual ~ShellOutput();
+
+        virtual void output() const = 0;
+
+        uint8_t getID() const;
+
+    private:
+        uint8_t ID;
+    };
+
+
+    class Shell{
+    public:
+        Shell(std::shared_ptr<ConsoleWindow> = nullptr);
+        ~Shell();
+
+        bool readRCFile         (const std::string& path);
+        bool addCommand         (CommandPtr command);              // Adds to commands unoredered_map
+        bool handleCommandQueue ();
+
+        bool addOutput(ShellOutputPtr output);
+        bool removeOutput(uint8_t getID);
+
+        void addToQueue         (const std::string& entry);
+
+        std::shared_ptr<ConsoleWindow> getWindow();
+
+    private:
+        std::unordered_map<std::string, CommandPtr> commands;
+        std::queue<std::string>                     commandQueue;
+
+        std::vector<ShellOutputPtr>                 outputs;
+
+        std::shared_ptr<ConsoleWindow>              window;
+    };
+}
