2ce5c9f1b2f1d94ae50fc08de37d7d6090b84866
Bradley Ryan <ryan.bradley.e@gmail.com>
2024-07-14 18:00:21 -0700

    Implemented correct pure virutal function for base Command class

--- SDK-Shared-Lib/src/Commands.cpp
+++ SDK-Shared-Lib/src/Commands.cpp
@@ -17,13 +17,4 @@ std::string Command::getDescription() const {
     return description;
 }
 
-std::pair<int, std::ostringstream> Command::execute() const {
-    throw std::runtime_error{"Invalid command execution state."};
-    std::pair<int, std::ostringstream> returnVal;
-    returnVal.first = 0;
-    returnVal.second << "Invalid command execution state.";
-    return returnVal;
-}
-
-
 
