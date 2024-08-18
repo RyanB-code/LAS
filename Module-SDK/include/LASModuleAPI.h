350c1c24f2cb5fd395e8e31c1e4042ab65ca052e
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2024-08-17 20:19:00 -0700

    Added annotations to functions

--- Module-SDK/include/LASModuleAPI.h
+++ Module-SDK/include/LASModuleAPI.h
@@ -15,7 +15,7 @@
     #define MODULE_EXPORT extern "C" __attribute__ ((visibility ("protected")))
 #endif
 
-MODULE_EXPORT bool      LASM_loadModuleInfo         (LAS::Information::ModuleInfo&);
-MODULE_EXPORT bool      LASM_loadEnvironmentInfo    (const LAS::Information::EnvironmentInfo&);
-MODULE_EXPORT void      LASM_cleanup    ();
+MODULE_EXPORT bool      LASM_loadModuleInfo         (LAS::Information::ModuleInfo&);            // Overwrite the parameter with your module's information
+MODULE_EXPORT bool      LASM_loadEnvironmentInfo    (const LAS::Information::EnvironmentInfo&); // This is information given by the environment to you to do what you want
+MODULE_EXPORT void      LASM_cleanup                ();                                         // Called upon closing of your module
 
