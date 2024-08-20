156b3906b5c57e967986732d8fda5687bb78a6cf
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2024-08-20 13:05:59 -0700

    Set variables in one location. easier to change for testing

--- Module-SDK/src/LASModuleAPI.cpp
+++ Module-SDK/src/LASModuleAPI.cpp
@@ -1,10 +1,24 @@
 #include <LASModuleAPI.h>
 
+using namespace LAS;
+using namespace LAS::Information;
+using namespace LAS::Windowing;
+using namespace LAS::Logging;
+
 LoggerPtr logger {};
 
-class TestWindow : public LAS::Window{
+const std::string TITLE                 { "Normal Window"};
+const std::string LOG_TEXT              { "Logging from module"};
+const std::string COMMAND_GROUP         { "normal-group"};
+const std::string COMMAND_KEY           { "command"};
+const std::string COMMAND_DESCRIPTION   { "description"};
+const std::string COMMAND_MSG           { "This is what is displayed after command executes.\nCommand executed from [" + TITLE + "].\n"};
+const Version     sdkVersion            {SDK::getVersionMajor(), SDK::getVersionMinor(), SDK::getVersionPatch()};
+
+
+class TestWindow : public Window{
 public:
-    TestWindow() : LAS::Window("Testing Window"){
+    TestWindow() : Window(TITLE){
 
     }
     ~TestWindow(){
@@ -14,10 +28,10 @@ public:
     void draw() override{
         ImGui::Begin(title.c_str(), &shown);
 
-        ImGui::Text("New Module Helper");
+        ImGui::Text("Text here");
 
         if(ImGui::Button( "Log" ))
-            logger->log("From Module", Tags{"Module"});
+            logger->log(LOG_TEXT, Tags{TITLE});
 
         ImGui::End();
     }
@@ -28,20 +42,22 @@ TestWindow window{ };
 
 class ModuleTestCommand : public Command {
 public:
-    ModuleTestCommand() : Command{"ModuleTest", "For Testing"} { }
+    ModuleTestCommand() : Command{COMMAND_KEY, COMMAND_DESCRIPTION} { }
     ~ModuleTestCommand() {}
 
     std::pair<int, std::ostringstream> execute (const std::vector<std::string>& args) override{
         std::pair<int, std::ostringstream> retVal;
         retVal.first = 0;
-        retVal.second << "Executed from module Testing 2\n";
+        retVal.second << COMMAND_MSG;
         return retVal;
     }
 };
 
 bool LASM_loadModuleInfo(ModuleInfo& moduleInfo){
-    moduleInfo.title                = "Testing 2";
-    moduleInfo.commandGroupName     = "test2";
+
+    moduleInfo.sdkVersion           = sdkVersion;
+    moduleInfo.title                = TITLE;
+    moduleInfo.commandGroupName     = COMMAND_GROUP;
     moduleInfo.window               = std::make_shared<TestWindow>(window);
 
     // Adding commands
@@ -52,13 +68,16 @@ bool LASM_loadModuleInfo(ModuleInfo& moduleInfo){
  bool LASM_loadEnvironmentInfo    (const EnvironmentInfo& envInfo){
     logger = envInfo.logger;
 
-    std::cout << "Directory: " << envInfo.directory << "\n";
+    std::cout << "From [" << TITLE << "]:\n";
+    std::cout << "\tDirectory: " << envInfo.directory << "\n";
+    std::cout << "\tRC File Path: " << envInfo.rcFilePath << "\n";
+
     ImGui::SetCurrentContext(&envInfo.context);
 
     return true;
  }
 
 void LASM_cleanup(){
-    std::cout << "From TestModule - In Cleanup\n";
+    std::cout << "Cleanup from [" << TITLE << "]\n";
     return;
 }
