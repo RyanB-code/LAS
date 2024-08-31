77fedaea276dcebd11f6b5cac9bce352d92d3035
Bradley Ryan <ryan.bradley.e@gmail.com>
2024-08-30 18:58:22 -0700

    Renamed ModuleAPI call to init after loadModuleInfo is accepted

--- ModuleAPI/src/LASModuleAPI.h
+++ ModuleAPI/src/LASModuleAPI.h
@@ -13,6 +13,6 @@
 #endif
 
 MODULE_EXPORT bool      LASM_loadModuleInfo         (LAS::Information::ModuleInfo&);            // Overwrite the parameter with your module's information
-MODULE_EXPORT bool      LASM_loadEnvironmentInfo    (const LAS::Information::EnvironmentInfo&); // This is information given by the environment to you to do what you want
+MODULE_EXPORT bool      LASM_init                   (const LAS::Information::EnvironmentInfo&); // This is information given by the environment to you to do what you want
 MODULE_EXPORT void      LASM_cleanup                ();                                         // Called upon closing of your module
 
