c55f4f776f1fd8b31540d68c2fc6d6d1281c0927
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2024-09-16 14:51:42 -0700

    Made ammo tracker class, does everything ammo related

--- Modules/Shooter-Central/src/Ammo.cpp
+++ Modules/Shooter-Central/src/Ammo.cpp
@@ -2,34 +2,195 @@
 
 using namespace ShooterCentral;
 
-// MARK: CARTRIDGE TYPE
-CartridgeType::CartridgeType(std::string setName) : name {setName}
+// MARK: AMMO TRACKER
+AmmoTracker::AmmoTracker(LAS::Logging::LoggerPtr setLogger): logger { setLogger }
 {
 
 }
-CartridgeType::~CartridgeType(){
-
+AmmoTracker::~AmmoTracker(){
+    
 }
-std::string CartridgeType::getName() const {
-    return name;
+// MARK: STOCKPILE
+bool AmmoTracker::addAmmoToStockpile (uint64_t amount, const std::string key){
+    if(ammoStockpile.contains(key)){
+        ammoStockpile.at(key)->amount += amount;
+        return true;
+    }
+    else
+        return false;    
+}
+bool AmmoTracker::addNewAmmoToStockpile (AmmoPtr ammo){
+    if(ammoStockpile.contains(ammo->name))
+        return false;
+
+    ammoStockpile.try_emplace(ammo->name, ammo);
+
+    if(ammoStockpile.contains(ammo->name)){
+        // Add to cartridges, does not matter return
+        addCartridge(ammo->cartridge);
+        return true;
+    }
+    else
+        return false;
 }
 
-Cartridge_9MM::Cartridge_9MM() : CartridgeType("9mm"){
+bool AmmoTracker::removeAmmoFromStockPile (uint64_t amountUsed, const std::string& key){
+    if(!ammoStockpile.contains(key))
+        return true;
+    
+    auto& ammo {ammoStockpile.at(key)};
 
+    if(ammo->amount - amountUsed >= 100000)
+        return false;
+    else
+        ammo->amount -= amountUsed;
+
+    return true;
 }
-Cartridge_9MM::~Cartridge_9MM(){
+bool AmmoTracker::addCartridge (const std::string& cartridge){
+    if(cartridges.contains(cartridge))
+        return false;
 
-}
-Cartridge_556::Cartridge_556() : CartridgeType("5.56x45mm"){
-
-}
-Cartridge_556::~Cartridge_556(){
-
-}
-Cartridge_300WM::Cartridge_300WM() : CartridgeType("300 Win Mag"){
-
-}
-Cartridge_300WM::~Cartridge_300WM(){
-
+    cartridges.try_emplace(cartridge, cartridge);
+    return cartridges.contains(cartridge);
 }
 
+// MARK: R/W AMMO
+bool AmmoTracker::writeAllAmmo() const{
+    using LAS::json;
+
+    if(ammoStockpile.empty())
+        return true;
+
+    if(!std::filesystem::exists(saveDirectory)){
+        logger->log("Directory [" + saveDirectory + "] was not found. Did not attempt to save any Ammo objects.", LAS::Logging::Tags{"ERROR", "SC"});
+        return false;
+    }
+
+
+	for(const auto& pair : ammoStockpile) {
+        const auto& ammo = *pair.second;
+
+        if(!AmmoHelper::writeAmmo(saveDirectory, ammo)) 
+            logger->log("Directory [" + saveDirectory + "] was not found. Ammo [" + ammo.name + "] was not saved.", LAS::Logging::Tags{"ERROR", "SC"});
+	}
+
+    return true;
+}
+// MARK: R/W CARTRIDGE
+bool AmmoTracker::writeAllCartridges() const{
+    if(!std::filesystem::exists(saveDirectory))
+        return false;
+
+    std::string fullPath { saveDirectory };
+    fullPath += cartridgesFile;
+
+    std::vector<std::string> rawCartridges;
+
+    for(auto& c : cartridges){
+        rawCartridges.push_back(c.second);
+    }
+
+    return AmmoHelper::writeAllCartridges(fullPath, rawCartridges);
+}
+std::string AmmoTracker::getDirectory() const{
+    return saveDirectory;
+}
+bool AmmoTracker::setDirectory(std::string path) {
+    path = LAS::TextManip::ensureSlash(path);
+
+    if(std::filesystem::exists(path)){
+        saveDirectory = path;
+        return true;
+    }
+    else
+        return false;
+}
+
+// MARK: AMMO HELPER
+bool AmmoHelper::writeAmmo(std::string directory, const Ammo& ammo){
+    using LAS::json;
+
+    if(directory.empty())
+       return false;
+
+    directory = LAS::TextManip::ensureSlash(directory);
+
+    if(!std::filesystem::exists(directory))
+		return 2;
+    
+
+    // Make JSON
+    json j;
+    j["name"]           = ammo.name;
+    j["manufacturer"]   = ammo.manufacturer;
+    j["cartridge"]      = ammo.cartridge;
+    j["grain"]          = int{ammo.grainWeight};
+    j["amountOnHand"]   = ammo.amount;
+
+
+    // Create JSON file name
+    std::string fileName;
+    for(const auto& c : ammo.name){     // Remove spaces and make lowercase
+        if(isalpha(c))
+            fileName += tolower(c);
+        else if(c == ' ' || c == '\t')
+            fileName += '-';
+        else if(isalnum(c))
+            fileName += c;
+    }
+    fileName += ".json";
+
+    // Make fully qualified path
+    std::string filePath;
+    filePath = directory + fileName;
+
+    // Write to file
+    std::ofstream file{filePath};
+    file << std::setw(1) << std::setfill('\t') << j;
+    file.close();
+   
+    return true;
+}
+Ammo AmmoHelper::readAmmo(const std::string& path){
+    using LAS::json;
+
+    if(!std::filesystem::exists(path))
+        throw std::filesystem::filesystem_error::runtime_error{"File does not exist"};
+
+    std::ifstream inputFile{ path, std::ios::in };
+    json j = json::parse(inputFile);
+
+    std::string nameBuf, manBuf, cartNameBuf; 
+    uint8_t grainBuf;
+    uint64_t amountBuf;
+
+    j.at("name").get_to(nameBuf);
+    j.at("manufacturer").get_to(manBuf);
+    j.at("cartridge").get_to(cartNameBuf);
+    j.at("grain").get_to(grainBuf);
+    j.at("amountOnHand").get_to(amountBuf);
+
+    return Ammo{nameBuf, manBuf, cartNameBuf, grainBuf, amountBuf };
+}
+bool AmmoHelper::writeAllCartridges(std::string path, const std::vector<std::string>& cartridges){
+    using LAS::json;
+
+    if(cartridges.empty())
+        return true;
+
+    json j;
+    json entries = nlohmann::json::array();
+
+	for (const auto& cartridge : cartridges) 
+        entries.push_back(cartridge);
+    
+    j["cartridges"] = entries;
+
+    // Write to file
+    std::ofstream file{path};
+    file << std::setw(1) << std::setfill('\t') << j;
+    file.close();
+
+    return true;
+}
--- Modules/Shooter-Central/src/Ammo.h
+++ Modules/Shooter-Central/src/Ammo.h
@@ -1,44 +1,59 @@
 #pragma once
 
+#include <LAS/json.h>
+#include <LAS/HelperFunctions.h>
+#include <LAS/Logging.h>
+
 #include <stdint.h>
 #include <string>
 #include <memory>
+#include <map>
+#include <vector>
 
-namespace ShooterCentral{  
-
-    // MARK: AMMO INFO
-    class CartridgeType {
-    public:
-        CartridgeType(std::string setName);
-        virtual ~CartridgeType();
-
-        std::string getName() const;
-
-    private:
-        std::string name;
-    };
-    class Cartridge_9MM : public CartridgeType {
-    public:
-        Cartridge_9MM();
-        ~Cartridge_9MM();
-    };
-    class Cartridge_556 : public CartridgeType {
-    public:
-        Cartridge_556();
-        ~Cartridge_556();
-    };
-    class Cartridge_300WM : public CartridgeType {
-    public:
-        Cartridge_300WM();
-        ~Cartridge_300WM();
-    };
+namespace ShooterCentral{
 
     struct Ammo {
         std::string     name;
         std::string     manufacturer;
-        CartridgeType   cartridgeType;     
+        std::string     cartridge;     
         uint8_t         grainWeight;
+        uint64_t        amount;
     };
 
     using AmmoPtr = std::shared_ptr<Ammo>;
+
+    class AmmoTracker{
+    public:
+        AmmoTracker(LAS::Logging::LoggerPtr setLogger);
+        ~AmmoTracker();
+
+        bool    addAmmoToStockpile      (uint64_t amount,   const std::string key);
+        bool    addNewAmmoToStockpile   (AmmoPtr ammo);
+        bool    removeAmmoFromStockPile (uint64_t amount,   const std::string& key);
+
+        bool    addCartridge            (const std::string& cartridge);
+
+        bool    writeAllAmmo            () const;
+        bool    writeAllCartridges      () const;
+
+        bool        setDirectory        (std::string directory);
+        std::string getDirectory        () const;
+
+    private:
+        std::map<std::string, AmmoPtr>     ammoStockpile;
+        std::map<std::string, std::string> cartridges;
+        std::string saveDirectory;
+        LAS::Logging::LoggerPtr logger;
+
+        static constexpr std::string cartridgesFile {"Cartridges.json"};
+    };
+
+    namespace AmmoHelper{
+        bool            writeAmmo               (std::string directory, const Ammo& ammo);
+        Ammo            readAmmo                (const std::string& path);
+
+        bool            writeAllCartridges      (std::string path, const std::vector<std::string>& cartridges);
+        StringVector    readCartridges          (std::string directory);
+    }
+
 }
