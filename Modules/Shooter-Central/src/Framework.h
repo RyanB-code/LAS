c750fa7c09e62794c2a3219db4e35c4882ccdcb5
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2024-09-16 14:52:40 -0700

    Moved file work into Framework, similar to LAS::Framework

--- Modules/Shooter-Central/src/Framework.h
+++ Modules/Shooter-Central/src/Framework.h
@@ -5,7 +5,6 @@
 #include "Drill.h"
 #include "Event.h"
 #include "Window.h"
-#include "FileIO.h"
 
 #include <LAS/Logging.h>
 
@@ -20,6 +19,13 @@ namespace ShooterCentral{
         Framework();
         ~Framework();
 
+        struct SubDirectories{
+            std::string ammoDir;
+            std::string drillsDir;
+            std::string eventsDir;
+            std::string gunsDir;
+        };
+
         bool setup(LAS::Logging::LoggerPtr setLoggerPtr, const std::string& directory);   // Call inside LASM_init
 
         bool addGun         (GunPtr gun);
@@ -30,10 +36,7 @@ namespace ShooterCentral{
 
         bool addEvent       (EventPtr event);
         bool removeEvent    (const std::string& key);
-         
-        bool addAmmoToStockpile         (uint64_t amount,   AmmoPtr ammo);
-        bool removeAmmoFromStockPile    (uint64_t amount,   const std::string& key);
-
+        
         SCWindowPtr getWindow() const;
         std::string getCommandGroupName() const;
 
@@ -44,15 +47,12 @@ namespace ShooterCentral{
         std::map<std::string, EventPtr>                         events;
         std::map<std::string, DrillPtr>                         drills;
         std::map<std::string, GunPtr>                           guns;
-        std::map<std::string, std::pair<uint64_t, AmmoPtr>>     ammoStockpile;
-
-        FileIOPtr fileIO;
 
         SCWindowPtr             window;
         LAS::Logging::LoggerPtr logger;
 
         static constexpr  std::string commandGroupName { "sc" };
 
-        bool setupFileIO(std::string directory);
+        bool setupFilesystem(std::string directory);
     };
 }
