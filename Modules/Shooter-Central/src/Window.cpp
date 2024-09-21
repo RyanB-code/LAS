9dad21a5ae5554e9e3af1a4328b249c3896b6175
Bradley Ryan <ryan.bradley.e@gmail.com>
2024-09-20 18:17:33 -0700

    Made separate function to show stockpile info, working on showing detailed view

--- Modules/Shooter-Central/src/Window.cpp
+++ Modules/Shooter-Central/src/Window.cpp
@@ -12,21 +12,15 @@ ShooterCentralWindow::~ShooterCentralWindow(){
 }
 void ShooterCentralWindow::draw() {
     if(ImGui::Begin(title.c_str(), &shown)){
-        ImGui::Text("Working");
 
-        static StringVector ammoNames;
-        static StringVector cartridgeNames;
-        ammoTracker->getAllAmmoNames(ammoNames);
-        ammoTracker->getAllCartridgeNames(cartridgeNames);
+        if(!ammoTracker){
+            ImGui::Text("This module has not been setup yet...");
+            ImGui::End();
+            return;
+        }
 
-        ImGui::Text("Ammo: ");
-        for(const auto& s : ammoNames){
-            ImGui::BulletText(s.c_str());
-        }
-        ImGui::Text("Cartridge: ");
-        for(const auto& s : cartridgeNames){
-            ImGui::BulletText(s.c_str());
-        }
+        drawStockpile();
+
     }
     ImGui::End();
 }
@@ -36,4 +30,74 @@ bool ShooterCentralWindow::addAmmoTracker(AmmoTrackerPtr setAmmoTracker){
 
     ammoTracker = setAmmoTracker;
     return true;
+}
+
+// MARK: PRIVATE FUNCTIONS
+void ShooterCentralWindow::drawStockpile() const{
+    if(ImGui::BeginChild("Stockpile")){
+
+        static StringVector ammoNames;
+        static StringVector cartridgeNames;
+        static std::vector<std::pair<std::string, uint64_t>> countByCartridge;
+
+        ammoTracker->getAllAmmoNames(ammoNames);
+        ammoTracker->getAllCartridgeNames(cartridgeNames);
+        ammoTracker->getAmmoCountByCartridge(countByCartridge);
+
+        static bool detailedView { false };
+
+        ImGui::Checkbox("Detailed View", &detailedView);
+
+        if(detailedView){
+            static int highlilghtedItem;
+            const char* items[64];
+
+            int i { 0 };
+            for(const auto& s : cartridgeNames){
+                items[i] = s.c_str();
+                ++i;
+            }
+            ImGui::Combo("Cartridge", &highlilghtedItem, items, cartridgeNames.size());
+
+            // Show highlighted item detailed table
+
+        }
+
+        if(cartridgeNames.empty())
+            ImGui::Text("There are no tracked cartridges.");
+        else{
+            if(ImGui::BeginTable("Stockpile", 2, 
+                                                    ImGuiTableFlags_RowBg | ImGuiTableFlags_Borders |
+                                                    ImGuiTableRowFlags_Headers | ImGuiTableFlags_Resizable |
+                                                    ImGuiTableFlags_SizingFixedFit | ImGuiTableFlags_HighlightHoveredColumn |
+                                                    ImGuiTableFlags_NoHostExtendX
+                                                    ))
+            {
+                ImGui::TableSetupColumn("Cartridge",    NULL, 100);
+                ImGui::TableSetupColumn("Amount",       NULL, 100);
+                ImGui::TableHeadersRow();
+
+                // Display each item
+                for(auto itr { countByCartridge.begin() }; itr != countByCartridge.end(); ++itr){
+                    ImGui::TableNextRow();
+                    for (int column{0}; column < 2; ++column)
+                    {
+                        ImGui::TableSetColumnIndex(column);
+                        if(column == 0)
+                            ImGui::Text(itr->first.c_str());
+                        else{
+                            std::ostringstream text;
+                            text << itr->second;
+                            ImGui::Text(text.str().c_str());
+                        }
+                    }        
+                }
+
+                ImGui::EndTable();
+            } // End table
+        }
+               
+
+        ImGui::EndChild();
+    }
 }
