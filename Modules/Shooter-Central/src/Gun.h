fb3fe00ec34bbe43d9e0ce122752132cbc4c19bd
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2024-09-23 10:24:24 -0700

    Added gun IDs, changed how total rounds are calculated, added GunFactory to make new guns

--- Modules/Shooter-Central/src/Gun.h
+++ Modules/Shooter-Central/src/Gun.h
@@ -16,32 +16,42 @@ namespace ShooterCentral{
         PRECISION_RIFLE,
         NOT_APPLICABLE
     };
-
     class Gun final {
     public:
-        Gun(std::string setName, WeaponType setWeaponType, std::string setCartridge);
+        Gun(std::string setName, WeaponType setWeaponType, std::string setCartridge, uint16_t setID);
         ~Gun();
 
         std::string getName         () const;
         uint64_t    getRoundCount   () const;
         WeaponType  getWeaponType   () const;
         std::string getCartridge    () const;
+        uint16_t    getID           () const;
 
-        bool    addNewAmmoTypeToRoundCount  (TrackedAmmoPtr ammo);
-        bool    addToRoundCount             (const std::string& ammoName, uint64_t amount);
+        bool    addToRoundCount (TrackedAmmoPtr ammo);
+        bool    addToRoundCount (const std::string& ammoName, uint64_t amount);
 
         void    getAllAmmoUsed (std::vector<TrackedAmmo>& ammoUsed) const; // Clears vector before adding elements
 
     private:
         std::string name;
-        uint64_t    totalRounds;
         WeaponType  weaponType;
         std::string cartridge;
+        const uint16_t ID;
 
         std::unordered_map<std::string, TrackedAmmoPtr> ammoTracker;
     };
 
     using GunPtr = std::shared_ptr<Gun>;
+
+    class GunFactory final {
+    public:
+        GunFactory();
+        ~GunFactory();
+
+        Gun createGun (const std::string& name, WeaponType weaponType, std::string cartridge) const;
+    private:
+        static uint16_t gunIDNumber;
+    };
 
     class GunTracker{
     public:
@@ -51,22 +61,27 @@ namespace ShooterCentral{
         bool        setDirectory    (std::string directory);
         std::string getDirectory    () const;
 
-        uint64_t getGunTotal () const;
+        uint16_t    createPistol            (const std::string& name, const std::string& cartridge);    // Returns assigned ID, 0 means it was not added
+        uint16_t    createRifle             (const std::string& name, const std::string& cartridge);    // Returns assigned ID, 0 means it was not added
+        uint16_t    createPrecisionRifle    (const std::string& name, const std::string& cartridge);    // Returns assigned ID, 0 means it was not added
+        bool        removeGun               (const uint16_t& ID);
 
-        bool addGun     (GunPtr gun);
-        bool removeGun  (const std::string& key);
-
-        GunPtr getGun   (const std::string& key) const;
+        uint64_t    getGunTotal ()                              const;
+        GunPtr      getGun      (const uint16_t& ID)            const;
+        void        getAllGunIDs(std::vector<uint16_t>& list) const;  // Clears vector before adding elements 
 
         bool writeAllGuns   () const;
         bool readGuns       ();
 
     private:
-        LAS::Logging::LoggerPtr logger;
+        LAS::Logging::LoggerPtr     logger;
+        GunFactory                  gunFactory;
 
-        std::map<std::string, GunPtr> guns;
+        std::map<uint16_t, GunPtr> guns;
 
         std::string saveDirectory;
+
+        bool addGun(GunPtr gun);
     };
 
     using GunTrackerPtr = std::shared_ptr<GunTracker>;
