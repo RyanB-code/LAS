6a25f18f1dba520d867482ba71b2a71cb8a156c4
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2024-09-26 21:41:48 -0700

    Added option for differentiating between debug and releae mode naming and linking, updated error reporting for all areas

--- SDK/build/scripts/Link-Rename-Distribute.sh
+++ SDK/build/scripts/Link-Rename-Distribute.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mode=$1
+
 configFileName=../../src/Config.h
 
 # =======================================================
@@ -23,16 +25,23 @@ done
 
 cd ../../bin/
 
-shortFileName=libLAS-SDK.so # This is what ever the build shared object library is called
+# =======================================================
+# Set variables
+if [[ "$mode" = "-r" ]]; then
+    shortFileName=libLAS-SDK.so # This is what ever the build shared object library is called
+else
+    shortFileName=libLAS-SDK-DEV.so
+fi
+
 finalRealName=libLAS-SDK.so.$major.$minor.$patch
 finalSOName=libLAS-SDK.so.$major
 
 
 # =======================================================
 # Link object files into SO file
-g++ -fdiagnostics-color=always -shared -Wl,-soname,libLAS-SDK.so.$major -o libLAS-SDK.so ../build/intermediate/*.o 2>&1
+g++ -fdiagnostics-color=always -shared -Wl,-soname,libLAS-SDK.so.$major -o $shortFileName ../build/intermediate/*.o 2>&1
 if [[ $? -eq 0 ]]; then
-    echo "Linked object files successful"
+    echo "Linking object files successful"
 else
     echo "Failed to link object files into SO file"
     exit 2
@@ -42,10 +51,10 @@ fi
 # Rename the new so with the real name
 if [ -f $shortFileName ]; 
 then
-  mv $shortFileName $finalRealName
+    mv $shortFileName $finalRealName
 else
-   echo "Could not find $shortFileName."
-   exit -1
+    echo "Could not find $shortFileName."
+    exit -1
 fi
 
 # =======================================================
@@ -55,14 +64,44 @@ cd ~
 # Create link from binary to vanilla soname
 ln -s /mnt/NAS/1-Project-Related/Project-Source-Directories/LAS/SDK/bin/$finalRealName $shortFileName 1>/dev/null 2>&1
 if [[ $? -eq 0 ]]; then
-    echo "Linking successful"
+    echo "Linking into short name successful"
 else
     echo "Failed to link short name and real name"
     exit 2
 fi
 
+# =======================================================
+# If in release mode, move link to /usr/lib
+if [[ "$mode" = "-r" ]]; then
+    echo Performing Release mode tasks...
+   
+    # Copy to lib folder for compilation to Environment
+    cp  $shortFileName /mnt/NAS/1-Project-Related/Project-Source-Directories/LAS/Environment/lib/
+    if [[ $? -eq 0 ]]; then
+        echo "Copied to Environment/lib"
+    else
+        echo "Could not copy $shortname to Environment/lib"
+    fi
+
+    # Move lib to where it can be found by OS
+    sudo mv /mnt/NAS/1-Project-Related/Project-Source-Directories/LAS/SDK/bin/$finalRealName /usr/local/lib/LAS/$finalSOName 1>/dev/null 2>&1
+    if [[ $? -eq 0 ]]; then
+        echo "Library added to /usr/local/lib/LAS"
+    else
+        echo "Library not added to /usr/local/lib/LAS"
+        rm -rf $shortFileName 1>/dev/null 2>&1  # Delete .so from home directory when done
+        exit 1
+    fi
+    
+    rm -rf $shortFileName 1>/dev/null 2>&1  # Delete .so from home directory when done
+    exit 0
+else
+    echo Performing Debug mode tasks...
+fi
+
+# =======================================================
 # Copy to lib folder for compilation to Environment and Module-SDK
-cp  $shortFileName /mnt/NAS/1-Project-Related/Project-Source-Directories/LAS/Environment/lib/ 1>/dev/null 2>&1
+cp  $shortFileName /mnt/NAS/1-Project-Related/Project-Source-Directories/LAS/Environment/lib/
 if [[ $? -eq 0 ]]; then
     echo "Copied to Environment/lib"
 else
@@ -74,7 +113,7 @@ else
     exit 2
 fi
 
-cp  $shortFileName /mnt/NAS/1-Project-Related/Project-Source-Directories/LAS/Modules/Test-Module/lib/ 1>/dev/null 2>&1
+cp  $shortFileName /mnt/NAS/1-Project-Related/Project-Source-Directories/LAS/Modules/Test-Module/lib/
 if [[ $? -eq 0 ]]; then
     echo "Copied to Test-Module/lib"
 else
@@ -86,7 +125,7 @@ else
     exit 2
 fi
 
-cp  $shortFileName /mnt/NAS/1-Project-Related/Project-Source-Directories/LAS/Modules/Shooter-Central/lib/ 1>/dev/null 2>&1
+cp  $shortFileName /mnt/NAS/1-Project-Related/Project-Source-Directories/LAS/Modules/Shooter-Central/lib/
 if [[ $? -eq 0 ]]; then
     echo "Copied to Shooter-Central/lib"
 else
