3bd865dd8503cc045738271b90b51406b1683096
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2024-10-02 16:43:10 -0700

    Added script to setup environment before using CMake

---
+++ build/scripts/setup-build-env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create necessary directories
+mkdir bin
+mkdir bin/Environment
+mkdir bin/Environment/Debug
+mkdir bin/Environment/Release
+
+mkdir bin/SDK
+mkdir bin/SDK/Debug
+mkdir bin/SDK/Release
+
+cd build
+cmake -DDEBUG=ON ..
