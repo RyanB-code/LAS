765587610305e1adf5f0bdca6e12a56f544c30b8
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2025-12-18 20:25:41 -0800

    Made text buffer that loops over itself. Intended to be used with Shell and LogWindow

--- Environment/src/Helpers.cpp
+++ Environment/src/Helpers.cpp
@@ -2,5 +2,57 @@
 
 namespace LAS {
 
+TextBuffer::TextBuffer() {
+    memBlockStart = &textBuffer[0][0];
+    memBlockEnd = &textBuffer[MAX_LINES -1 ][MAX_CHAR_LINE];
+
+    firstLine = memBlockStart;
+    nextLine = firstLine;
+
+    memset(textBuffer, 0, sizeof(textBuffer));
+}
+void TextBuffer::push(const std::string& text) {
+    int lineSize { sizeof(textBuffer[0]) };
+
+    // Copy up to total allowed size
+    std::strncpy(nextLine, text.c_str(), lineSize - 1);
+    nextLine[lineSize - 1] = '\0';
+
+    incLine(&nextLine);
+
+    if(nextLine == firstLine){
+        incLine(&firstLine);
+    }
+}
+
+void TextBuffer::clear(){
+    memset(textBuffer, 0, sizeof(textBuffer));
+    firstLine = memBlockStart;
+    nextLine = firstLine;
+}
+void TextBuffer::writeToScreen() const {
+
+    char* lineToPrint   { firstLine };
+    char* nextPrintLine { lineToPrint };
+
+    incLine(&nextPrintLine);
+
+    while(nextPrintLine != firstLine) {
+
+        if(*lineToPrint != 0)
+            ImGui::Text(lineToPrint);
+
+        incLine(&lineToPrint);
+        incLine(&nextPrintLine);
+    }
+}
+void TextBuffer::incLine(char** line) const{
+    int lineSize { sizeof(textBuffer[0]) };
+
+    *line += lineSize;
+
+    if(*line >= memBlockEnd)
+       *line = memBlockStart;
+}
 
 }   // End LAS
--- Environment/src/Helpers.h
+++ Environment/src/Helpers.h
@@ -2,6 +2,8 @@
 
 #include <functional>
 #include <string>
+#include <imgui/imgui.h>
+#include <cstring>
 
 namespace LAS {
 
@@ -13,6 +15,32 @@ namespace LAS {
         const std::string tag;
         const std::function<void()> function;
         bool shown { false };
+    };
+
+    class TextBuffer {
+    public:
+        TextBuffer();
+        ~TextBuffer() = default;
+
+        void push(const std::string& text);
+        void clear();
+
+        void writeToScreen() const;
+
+        int getMaxCharPerLine() const { return MAX_CHAR_LINE - 1;  }
+    private:
+        static constexpr int MAX_CHAR_LINE  { 256 };
+        static constexpr int MAX_LINES      { 64 };
+
+        char textBuffer [MAX_LINES][MAX_CHAR_LINE];
+        
+        char* memBlockStart { nullptr };
+        char* memBlockEnd   { nullptr };
+
+        char* firstLine { nullptr };
+        char* nextLine  { nullptr };
+
+        void incLine(char** line) const;
     };
 }   // End LAS
 
